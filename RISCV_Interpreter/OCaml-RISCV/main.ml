3ce6d5a13a21db68c3c85d0c546ddf0c8f29fed0
Jonathan Tran <52905762+JonathanDLTran@users.noreply.github.com>
2020-03-30 00:27:37 -0400

    Add files via upload

--- RISCV_Interpreter/OCaml-RISCV/main.ml
+++ RISCV_Interpreter/OCaml-RISCV/main.ml
@@ -23,6 +23,7 @@ type riscv =
   | Label of string
   | Lw of string * string * int
   | Sw of string * string * int
+  | Sb of string * string * int
 
 (*
   | Jal of string * string (* First argument is the save register, second is the label location *)
@@ -34,7 +35,6 @@ type riscv =
   | Bltu of string * string * string
   | Bgeu of string * string * string
   | Lb of string * string * int
-  | Sb of string * string * int
 *)
 
 let rec init_reg_file counter n_regs rf =
@@ -56,15 +56,94 @@ let sltiu_helper =
 let xori_helper = 
   fun a b -> if b = -1 then lnot a else a lxor b
 
-let rec dec_to_base_2 dec acc = 
+(* In Little Endian order *)
+let rec dec_to_base_2 acc dec = 
   if dec < 0 then failwith "Base 10 number must be non-negative"
   else 
     let dec' = dec / 2 in 
     let rem = dec mod 2 in 
-    if dec' = 0 then List.rev (dec :: acc) 
-    else dec_to_base_2 dec' (rem :: acc)
+    if dec' = 0 then List.rev (dec :: acc)
+    else dec_to_base_2 (rem :: acc) dec'
 
+let rec pad_one_32_helper lst len = 
+  if len = 0 then lst 
+  else pad_one_32_helper (1 :: lst) (len - 1)
 
+(** Requires: [lst] is in little endian order *)
+let pad_one_32 lst = 
+  let len = List.length lst in 
+  let rem = 32 - len in 
+  List.rev (pad_one_32_helper (List.rev lst) rem)
+
+let rec pad_zero_32_helper lst len = 
+  if len = 0 then lst 
+  else pad_zero_32_helper (0 :: lst) (len - 1)
+
+(** Requires: [lst] is in little endian order *)
+let pad_zero_32 lst = 
+  let len = List.length lst in 
+  let rem = 32 - len in 
+  List.rev (pad_zero_32_helper (List.rev lst) rem)
+
+let sign_extend lst = 
+  if lst = [] then pad_zero_32 lst 
+  else begin 
+    let head = List.hd lst in 
+    if head = 1 then pad_one_32 lst 
+    else pad_zero_32 lst 
+  end
+
+(* Little endian constant *)
+let c_ONE_BASE_2 = 
+  1 |> dec_to_base_2 [] |> pad_zero_32
+
+let invert_base_2 lst = 
+  List.map (fun x -> if x = 1 then 0 else 1) lst
+
+(* Both lst1 and lst2 are in little endian order *)
+let rec add_base_2_helper lst1 lst2 carry acc = 
+  match lst1, lst2 with 
+  | [], [] -> List.rev acc 
+  | [], _ 
+  | _, [] -> failwith "Both lsts must be the same length"
+  | h1 :: t1, h2 :: t2 -> 
+    let digit = h1 + h2 + carry in 
+    if digit > 1 then add_base_2_helper t1 t2 1 (digit :: acc) 
+    else add_base_2_helper t1 t2 0 (digit :: acc)
+
+let add_base_2 lst1 lst2 = 
+  add_base_2_helper lst1 lst2 0 []
+
+(* n is decimal, and returns little endian list *)
+let convert_to_base_2 n = 
+  if n >= 0 then dec_to_base_2 [] n 
+  else 
+    let pos_n = abs n in 
+    let pos_base_2 = dec_to_base_2 [] pos_n in 
+    let inverted_base_2 = invert_base_2 pos_base_2 in 
+    add_base_2 inverted_base_2 c_ONE_BASE_2
+
+(* In little endian order, and returns little endian order *)
+let rec take_first_n lst rem_list acc n = 
+  if n = 0 then (List.rev acc, rem_list)
+  else begin
+    match lst with 
+    | [] -> failwith "List not long enough to remove n elements"
+    | h :: t -> 
+      take_first_n t t (h :: acc) (n - 1)
+  end
+
+(* Splits into little endian order *)
+let split_32_into_4_groups lst = 
+  assert (List.length lst = 32);
+  let lst1, rem1 = take_first_n lst [] [] 8 in 
+  let lst2, rem2 = take_first_n rem1 [] [] 8 in 
+  let lst3, rem3 = take_first_n rem2 [] [] 8 in 
+  let lst4, rem4 = take_first_n rem3 [] [] 8 in 
+  (lst1, lst2, lst3, lst4)
+
+let convert_to_base_10 lst = 
+  List.fold_left (fun acc elt -> 2 * acc + elt) 0 (List.rev lst)
 
 (** [dec_to_base_16 dec acc] converts [dec] in base 10 to
     base 16 hexadecimal in list form. 
@@ -162,6 +241,8 @@ and eval expr_lst rf ram pc =
   | Lw (rd, rs1, imm) :: t -> eval_lw rd rs1 imm rf ram; eval t rf ram (pc + 4)
   | Sw (rs1, rs2, imm) :: t -> eval_sw rs1 rs2 imm rf ram; eval t rf ram (pc + 4)
 
+  | Sb (rs1, rs2, imm) :: t -> eval_sb rs1 rs2 imm rf ram; eval t rf ram (pc + 4)
+
 and eval_auipc rd imm pc rf ram = 
   let a = imm lsl 12 in 
   let res = a + pc in 
@@ -198,19 +279,41 @@ and eval_lw rd rs1 imm rf ram =
   if address < 0 then failwith "Memory Fault : Cannot access negative address"
   else match Hashtbl.find_opt ram address with 
     | None -> Hashtbl.add rf rd 0 (* Assume memory id zeroed out, but UB allows me to do anything *)
-    | Some i -> Hashtbl.add rf rd i
+    | Some lst -> failwith "Unimplemented"
+
+and eval_lb rd rs1 imm rf ram = 
+  let a = Hashtbl.find rf rs1 in 
+  let address = a + imm in 
+  if address < 0 then failwith "Memory Fault : Cannot access negative address"
+  else match Hashtbl.find_opt ram address with 
+    | None -> Hashtbl.add rf rd 0 (* Assume memory id zeroed out, but UB allows me to do anything *)
+    | Some lst -> 
+      let extended_list = sign_extend lst in 
+      let dec = convert_to_base_10 extended_list in 
+      Hashtbl.add rf rd dec
 
 and eval_sw rs1 rs2 imm rf ram = 
   let a = Hashtbl.find rf rs1 in 
   let b = Hashtbl.find rf rs2 in 
-  let b_list = dec_to_base_16 b [] in (* b_list is base 16 in base 10 rep of the data in b *)
+  let b_list = convert_to_base_2 b in (* b_list is base 2, little endian first at head *)
+  let s1, s2, s3, s4 = split_32_into_4_groups b_list in  
   let address = a + imm in 
   if address < 0 then failwith "Memory Fault : Cannot access negative address"
   else 
-    let () = Hashtbl.add ram address b in (* temp solution, need to break up values *)
-    Hashtbl.add ram (address + 1) b;
-    Hashtbl.add ram (address + 2) b;
-    Hashtbl.add ram (address + 3) b
+    let () = Hashtbl.add ram address s1 in 
+    Hashtbl.add ram (address + 1) s2;
+    Hashtbl.add ram (address + 2) s3;
+    Hashtbl.add ram (address + 3) s4
+
+and eval_sb rs1 rs2 imm rf ram = 
+  let a = Hashtbl.find rf rs1 in 
+  let b = Hashtbl.find rf rs2 in 
+  let b_list = convert_to_base_2 b in (* b_list is base 2, little endian first at head *)
+  let s1, _ = take_first_n b_list [] [] 8 in  (* takes low bits of b_list *)
+  let address = a + imm in 
+  if address < 0 then failwith "Memory Fault : Cannot access negative address"
+  else Hashtbl.add ram address s1
+
 
 
 
